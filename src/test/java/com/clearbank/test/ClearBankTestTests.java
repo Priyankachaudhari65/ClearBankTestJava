c30c8725c678662537a93fd31f93e58f99ffe43b
priyankachaudhari65 <pgchaudhari65@gmail.com>
2021-10-20 16:38:56 +0100

    java code

---
+++ src/test/java/com/clearbank/test/ClearBankTestTests.java
@@ -0,0 +1,14 @@
+package com.clearbank.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearBankTestTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
